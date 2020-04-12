aa3ba83d8eff2bff9dda8d3bb27e5a7148812c30
Sviatoslav Hamar <sviatoslavgamar2001@gmail.com>
2020-04-12 15:16:35 +0300

    create project, create class command

---
+++ TuringMachine/Command.cs
@@ -0,0 +1,32 @@
+﻿namespace TuringMachine
+{
+    class Command
+    {
+        public string CurrState { get; set; }
+        public string CurrSymb { get; set; }
+        public string NewState { get; set; }
+        public string NewSymb { get; set; }
+        public char Direction { get; set; }
+        public Command()
+        {
+
+        }
+        public Command(string currState, string currSymb, string newState, string newSymb, char direction)
+        {
+            CurrState = currState;
+            CurrSymb = currSymb;
+            NewState = newState;
+            NewSymb = NewSymb;
+            Direction = direction;
+        }
+        public Command(string data)
+        {
+            string[] command = data.Split();
+            CurrState = command[0];
+            CurrSymb = command[1];
+            NewState = command[5];
+            NewSymb = command[3];
+            Direction = command[4][0];
+        }
+    }
+}
